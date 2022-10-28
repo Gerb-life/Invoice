834ab4e5b2305d67933aa9ec1a06d53028c26dbb
Kellan Anderson <kdanderson5@catamount.wcu.edu>
2022-10-27 22:58:21 -0400

    added tests.txt, create.sh final

--- create.sh
+++ create.sh
@@ -1,24 +1,18 @@
 #!/bin/bash
 
-# check in/out state -> set global var
-# ask for name
-# check name for non-numbers
-# ask for street address
-# check the street address
-# ask for city
-# check the city for non-numbers
-# if out of state -> ask for state
-#   check to see if state is non-num and is 2 characters long
-# ask for fields
-# loop through entered feilds and ask how many of each item they want to purchace -> must be a num
-# call valid -> print to std out
-# if vaild suceeds
-#   print file creation message
-#   loop through the first two lines of the file and echo to stdout
+###############################################################################
+# Kellan Anderson, Gabe Rodriguez
+# 10/27/2022
+#
+# Create file for project 2 
+# Gets user input for a shopping invoice script and makes sure that all of the
+# passed values are valid. It then creates a file and passes it to valid.sh 
+###############################################################################
 
-#Exit status refrences
+#Exit status legend
 STATUS_BAD_USAGE=1
 STATUS_INVALID_ARG=2
+STATUS_VALID_FAILED=3
 
 #################################### Name ####################################
 if [[ $# != 2 ]] ; then
@@ -30,31 +24,48 @@ fi
 echo -n "Please enter customer name > "
 read name
 
+# Make sure name is not empty
+if [[ $name == "" ]]; then 
+    echo "Cannot have an empty field"; 
+    exit $STATUS_INVALID_ARG; 
+fi
+
 # Test to make sure there are no numbers in the customer name
 test_name=`echo $name | grep -o -e "[0-9]*"`
-if [[ $test_name != "" ]]; then echo "Invalid name"; exit $STATUS_INVALID_ARG; fi
+if [[ $test_name != "" ]]; then 
+    echo "Invalid name"; 
+    exit $STATUS_INVALID_ARG; 
+fi
 
 
 ################################### Address ###################################
 
 # get the address
 echo -n "Please enter the street address > "
-read street_num street_name_1 street_name_2 street_id
+read street_num street
 
-# look to see if the the street has more than one name, if not then reassign the
-# street identifier
-if [[ $street_id == "" ]]; then street_id=$street_name_2; street_name_2=""; fi 
+# Make sure address is not empty
+if [[ $street_num == "" ]]; then 
+    echo "Cannot have an empty field"; 
+    exit $STATUS_INVALID_ARG; 
+fi
 
 # Make sure the street number is a number
 test_street_num=`echo $street_num | grep -o -e "[0-9]*"`
-if [[ $test_street_num == "" ]]; then echo "invalid street number"; exit $STATUS_INVALID_ARG; fi
+if [[ $test_street_num == "" ]]; then 
+    echo "Invalid street number"; 
+    exit $STATUS_INVALID_ARG; 
+fi
 
 # make sure that the street name and identifier are valid
-test_string=`echo $street_name_1$street_name_2$street_id | grep -o -e "[0-9]*"`
-if [[ $test_string != "" ]]; then echo "Invalid street name or identifier"; exit $STATUS_INVALID_ARG; fi
+test_string=`echo $street | grep -o -e "[0-9]*"`
+if [[ $test_string != "" ]]; then 
+    echo "Invalid street name"; 
+    exit $STATUS_INVALID_ARG; 
+fi
 
 # Format the street address into one variable
-address=`echo "$street_num $street_name_1 $street_name_2 $street_id"`
+address=`echo "$street_num $street"`
 
 ##################################### City ####################################
 
@@ -62,9 +73,19 @@ address=`echo "$street_num $street_name_1 $street_name_2 $street_id"`
 echo -n "Please enter city > "
 read city
 
-# Test to make sure the city name is not an empty string and there are not numbers in it
+# Make sure that the city is not empty
+if [[ $city == "" ]]; then 
+    echo "Cannot have an empty field"; 
+    exit $STATUS_INVALID_ARG; 
+fi
+
+# Test to make sure the city name is not an empty string and there are not 
+# numbers in it
 test_city=`echo $city | grep -o -e "[0-9]*"`
-if [[ $test_city != "" ]] || [[ $city == "" ]]; then echo "Invlid city name"; exit $STATUS_INVALID_ARG; fi
+if [[ $test_city != "" ]] || [[ $city == "" ]]; then 
+    echo "Invalid city name"; 
+    exit $STATUS_INVALID_ARG; 
+fi
 
 
 #################################### State ####################################
@@ -80,7 +101,10 @@ else
 
     #Test the state
     test_state=`echo $state | grep -ox -e "[^0-9]*" | wc -c`
-    if [[ $test_state != 3 ]]; then echo "Invalid state"; exit $STATUS_INVALID_ARG; fi
+    if [[ $test_state != 3 ]]; then 
+        echo "Invalid state"; 
+        exit $STATUS_INVALID_ARG; 
+    fi
 fi
 
 ############################## Read Categories ################################
@@ -88,6 +112,12 @@ fi
 # Get the order fields
 echo -n "Please enter the number of fields that compromise the order > "
 read fields
+
+# Make sure that the fields entered are not empty
+if [[ $fields == "" ]]; then 
+    echo "Cannot have an empty field"; 
+    exit $STATUS_INVALID_ARG; 
+fi
 
 # String to to concatenate the output string
 purchased=""
@@ -98,22 +128,32 @@ for field in $fields; do
     echo -n "Please enter the number of \"$field\" items you want to purchace > "
     read num
 
+    # Make sure that the number of the item purchased is not empty
+    if [[ $num == "" ]]; then 
+        echo "Cannot have an empty field"; 
+        exit $STATUS_INVALID_ARG; 
+    fi
+
     # Check to make sure the number of items purchaced is a number
-    test_num=`echo $num | grep -o -e "[0-9]*"`
-    if [[ $num == "" ]]; then echo "Invalid argument given, must be a number"; exit $STATUS_INVALID_ARG; fi
+    test_num=`echo -n $num | grep -oe "[a-zA-Z]"`
+    if [[ $test_num != "" ]]; then 
+        echo "Invalid argument given, must be a number"; 
+        exit $STATUS_INVALID_ARG; 
+    fi
     
     purchased=`echo -n "$purchased$num "`
 
 done
 
-########## Formatting ################
+################################# Formatting ##################################
 
-# Remove the trailing comma and format the items purchased
+# Remove the trailing comma and format
 purchased=`echo $purchased | awk -F" " 'BEGIN{OFS=",";} {$1=$1; print $0;}'`
-
 cats=`echo $fields | awk -F" " 'BEGIN{OFS=",";} {$1=$1; print $0;}'`
 
-############## Call Valid.sh ########################
+############################### Call Valid.sh #################################
+
+# Get the correct extention
 ext=""
 if [[ $1 == "-i" ]] ; then
     ext=".iso"
@@ -121,25 +161,27 @@ else
     ext=".oso"
 fi
 
+# Get the filename
 ext=`echo "$2$ext"`
 
-echo "customer: $name" >> $ext
+# Populate the file
+echo "customer: $name" > $ext
 echo "address: $address, $city, $state" >> $ext
 echo "categories: $cats" >> $ext
 echo "items: $purchased" >> $ext 
 
+# Call valid.sh
 echo "Calling valid with $ext"
 bash valid.sh $ext
 if [[ $? != 0 ]] ; then
+    # remove the file if invalid and exit
     rm $ext
-    exit 1
+    exit $STATUS_VALID_FAILED
 fi
 
+# Echo the success
+echo ""
 echo "\"$ext\" has been created for "
 head -n2 $ext
 
-################################## Testing ####################################
-#echo "customer:$name"
-#echo "address:$address, $city, $state"
-#echo "categories:$cats"
-#echo "Items:$purchased"
+#################################### EOF ######################################
